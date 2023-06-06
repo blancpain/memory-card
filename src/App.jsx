da360ba9eeec77ae092219647e335c939d820a9f
Yasen Dimitrov <y_dimitrov@ymail.com>
2023-06-06 09:49:37 +0300

    Set up folder structure and connect to API for test run

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import { nanoid } from "nanoid";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [pics, setPics] = useState([]);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  useEffect(() => {
+    async function getPics() {
+      const response = await fetch("http://shibe.online/api/shibes?count=20");
+      const data = await response.json();
+      setPics(data);
+    }
+    getPics();
+  }, []);
+
+  const allPics = pics.map((pic) => {
+    return <img src={pic} key={nanoid()} className="card" alt="dog" />;
+  });
+  return <main>{allPics}</main>;
 }
 
-export default App
+export default App;
